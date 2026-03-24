797517a4a395f88d90ab3f9bc9f524e870822a6d
Kivvosh <oliwkaniesiel@gmail.com>
2026-03-24 10:02:36 +0100

    24.03.2026 update
    Project added to reporitory. 2 placeholders and created script are included

---
+++ Assets/first_script.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class first_script : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
